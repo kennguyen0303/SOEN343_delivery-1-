6eff784268b7dda814fc5bf7b36ab1360a480a5c
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-10-18 11:43:19 -0400

    Add isLoggedUser property to User

--- demo/src/main/java/com/soen343/backend/model/User.java
+++ demo/src/main/java/com/soen343/backend/model/User.java
@@ -8,21 +8,28 @@ import javax.validation.constraints.NotBlank;
 public class User {
 
     private final UUID id;
+    private boolean isLoggedUser = false;
 
     @NotBlank
     private final String role;
+
+    public User(@JsonProperty("id") UUID id, @JsonProperty("role") String role) {
+        this.id = id;
+        this.role = role;
+    }
 
     public String getRole() {
         return role;
     }
 
-    public User(@JsonProperty("id") UUID id, @JsonProperty("role") String role)
-    {
-        this.id = id;
-        this.role = role;
+    public boolean getIsLoggedUser() {
+        return isLoggedUser;
     }
 
+
+
     public UUID getId() {
+
         return id;
     }
 }
