d76bda279543521648a6bad71ad3c108d37a4c75
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-10-19 17:35:22 -0400

    Add Java doc comments to UserDataAccessService

--- demo/src/main/java/com/soen343/backend/dao/UserDataAccessService.java
+++ demo/src/main/java/com/soen343/backend/dao/UserDataAccessService.java
@@ -17,10 +17,10 @@ public class UserDataAccessService implements UserDAO {
     private List<User> DB = new ArrayList<>();
 
     /**
-     *
-     * @param id
-     * @param user
-     * @return
+     * Adds a new User to the database list
+     * @param id Unique Id that is assigned to the new user
+     * @param user A User Model to insert into the database list
+     * @return an integer. 1 if the operation of succesful and 0 if it fails
      */
     @Override
     public int insertUser(UUID id, User user) {
@@ -29,14 +29,19 @@ public class UserDataAccessService implements UserDAO {
     }
 
     /**
-     *
-     * @return
+     *  A Getter for All Users models currently stored
+     * @return A List of All Users registered
      */
     public List<User> selectAllUsers() {
 
         return DB;
     }
 
+    /**
+     * A Getter to find a specific user with their id
+     * @param id Unique Id to identify the user to select
+     * @return An Optional User if found with the provided id
+     */
     @Override
     public Optional<User> selectUserById(UUID id) {
         return DB.stream()
@@ -45,9 +50,9 @@ public class UserDataAccessService implements UserDAO {
     }
 
     /**
-     *
-     * @param id
-     * @return
+     * Deletes the specific user identified by an id
+     * @param id Unique Id to identify the user to delete
+     * @return an Integer, 1 if no user was found and the operation fails or 1 if the operating is successful
      */
     @Override
     public int deleteUserById(UUID id) {
@@ -63,10 +68,10 @@ public class UserDataAccessService implements UserDAO {
     }
 
     /**
-     *
-     * @param id
-     * @param updateUser
-     * @return
+     * An method to update the role of a user that already exists
+     * @param id Unique Id to identify the user to update
+     * @param updateUser a user Model with new values to replace the old one with
+     * @return an Integer, 1 if no user was found and the operation fails or 1 if the operating is successful
      */
     @Override
     public int updateUserById(UUID id, User updateUser) {
@@ -84,9 +89,9 @@ public class UserDataAccessService implements UserDAO {
     }
 
     /**
-     *
-     * @param id
-     * @return
+     * Logs in user with specified Id and logs out the currently logged user
+     * @param id Unique Id to identify the user to update
+     * @return an Integer, 1 if no user was found and the operation fails or 1 if the operating is successful
      */
     @Override
     public int loginUser(UUID id) {
@@ -112,8 +117,8 @@ public class UserDataAccessService implements UserDAO {
     }
 
     /**
-     *
-     * @return
+     * Finds the User such that their current logged in status is true
+     * @return an Optional User if it is found, if any user is logged in
      */
     private Optional<User> findCurrentLoggedInUser() {
         return DB.stream()
@@ -122,10 +127,10 @@ public class UserDataAccessService implements UserDAO {
     }
 
     /**
-     *
-     * @param id
-     * @param location
-     * @return
+     * Updated the Location of a specific user
+     * @param id Unique Id to identify the user to update
+     * @param location A String representing their new location
+     * @return an Integer, 1 if no user was found and the operation fails or 1 if the operating is successful
      */
     @Override
     public int setUserLocation(UUID id, String location) {
