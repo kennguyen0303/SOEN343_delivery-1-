0e5e94bf947d4e5ba3f0229b7e326f815bc74d11
NathanZiri <37674081+NathanZiri@users.noreply.github.com>
2020-10-20 10:37:43 -0400

    Update function.js

--- demo/src/main/resources/static/function.js
+++ demo/src/main/resources/static/function.js
@@ -372,19 +372,7 @@ function openForm() {
 
 
 function startTime() {
-	var month = new Array();
-	month[0] = "January";
-	month[1] = "February";
-	month[2] = "March";
-	month[3] = "April";
-	month[4] = "May";
-	month[5] = "June";
-	month[6] = "July";
-	month[7] = "August";
-	month[8] = "September";
-	month[9] = "October";
-	month[10] = "November";
-	month[11] = "December";
+	var month = ["January", "February", "March", "April","May", "June","July", "August", "September", "October", "November", "December"];
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
